b2eae9fb1d0a1f3d5e769ad1b71e07cabad042c4
JohnnyArvelo <metalarvelo@gmail.com>
2022-05-19 00:51:13 -0300

    Organizando Dist

---
+++ Practica.ts
@@ -0,0 +1,9 @@
+let usuario= { 
+    nombre:"johnny",
+    edad:22,
+    direccion:{calle:"San guillermo",altura:6942}
+    };
+let persona={ nombre:"Juan",edad:43};
+console.log(usuario);
+console.log(persona.nombre);
+console.log(usuario.direccion.calle);
